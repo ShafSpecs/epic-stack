21e4157da854af5c46321a8cd68a5cd1e4f360dc
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-04-10 12:05:47 +0100

    Added Remix PWA

---
+++ app/entry.worker.ts
@@ -0,0 +1,96 @@
+/// <reference lib="WebWorker" />
+
+import {
+  clearUpOldCaches,
+	EnhancedCache,
+	isDocumentRequest,
+	isLoaderRequest,
+	Logger,
+	NavigationHandler,
+	type DefaultFetchHandler,
+} from '@remix-pwa/sw'
+
+declare let self: ServiceWorkerGlobalScope
+
+const logger = new Logger({
+	prefix: '[Epic Stack]',
+})
+
+const version = 'v1'
+
+const DOCUMENT_CACHE_NAME = `document-cache`
+const ASSET_CACHE_NAME = `asset-cache`
+const DATA_CACHE_NAME = `data-cache`
+
+const documentCache = new EnhancedCache(DOCUMENT_CACHE_NAME, {
+	version,
+	strategy: 'CacheFirst',
+	strategyOptions: {
+		maxEntries: 64,
+	},
+})
+
+const assetCache = new EnhancedCache(ASSET_CACHE_NAME, {
+	version,
+	strategy: 'CacheFirst',
+	strategyOptions: {
+		maxAgeSeconds: 60 * 60 * 24 * 90, // 90 days
+		maxEntries: 100,
+	},
+})
+
+const dataCache = new EnhancedCache(DATA_CACHE_NAME, {
+	version,
+	strategy: 'NetworkFirst',
+	strategyOptions: {
+		networkTimeoutInSeconds: 10,
+		maxEntries: 72,
+	},
+})
+
+self.addEventListener('install', event => {
+	logger.log('Service worker installed')
+
+	event.waitUntil(Promise.all([
+    assetCache.preCacheUrls(
+      self.__workerManifest.assets.filter(url => !url.endsWith('.map') && !url.endsWith('.js'))
+    ),
+    self.skipWaiting(),
+  ]))
+})
+
+self.addEventListener('activate', event => {
+	logger.log('Service worker activated')
+
+	event.waitUntil(Promise.all([
+    clearUpOldCaches([DOCUMENT_CACHE_NAME,DATA_CACHE_NAME,ASSET_CACHE_NAME], version),
+    self.clients.claim(),
+  ]))
+})
+
+export const defaultFetchHandler: DefaultFetchHandler = async ({ context }) => {
+	const request = context.event.request
+	const url = new URL(request.url)
+
+	if (isDocumentRequest(request)) {
+		return documentCache.handleRequest(request)
+	}
+
+	if (isLoaderRequest(request)) {
+		return dataCache.handleRequest(request)
+	}
+
+	if (self.__workerManifest.assets.includes(url.pathname)) {
+		return assetCache.handleRequest(request)
+	}
+
+	return fetch(request)
+}
+
+const messageHandler = new NavigationHandler({
+  cache: documentCache
+})
+
+self.addEventListener('message', (event: ExtendableMessageEvent) => {
+  event.waitUntil(messageHandler.handleMessage(event))
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { remixPWA } from '@remix-pwa/dev'
 import { vitePlugin as remix } from '@remix-run/dev'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { glob } from 'glob'
@@ -58,5 +59,6 @@ export default defineConfig({
 					},
 				})
 			: null,
+    remixPWA()
 	],
 })
